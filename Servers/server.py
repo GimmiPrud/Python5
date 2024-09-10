0b83fecc67131462cb38b32b76d023c0cdffc3cd
GimmiPrud <prudenzig@gmail.com>
2024-09-10 17:28:24 +0200

    ,....

--- Servers/server.py
+++ Servers/server.py
@@ -6,6 +6,11 @@ api = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@api.route('/', methods = ['GET'])
+def indexx():
+    return render_template('index2.html')
+
 api.run(host="127.0.0.1",port=8085)
 
 
