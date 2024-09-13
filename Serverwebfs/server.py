6e8962db67664928fecce7b442c291c134376a72
GimmiPrud <prudenzig@gmail.com>
2024-09-13 17:17:36 +0200

    ...

--- Serverwebfs/server.py
+++ Serverwebfs/server.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 api = Flask(__name__)
 
-utenti = ['lucio','password1','M','0'],['gianni','passwoe','M','0'],['alessia','delaie','D','0']
+utenti = [['lucio','password1','M','0'],['gianni','passwoe','M','0'],['alessia','delaie','D','0']]
 
 @api.route('/', methods = ['GET'])
 def index():
@@ -22,6 +22,16 @@ def regok():
 @api.route('/', methods = ['GET'])
 def regko():
     return render_template('reg_ko.html')
+
+
+@api.route('/registrati', methods = ['GET'])
+def registra():
+    nome = request.args.get('nome')
+    print('Nome inserito', nome)
+    password = request.args.get('cognome')
+    # devo prendere tutti i dati e verificare se la terna c'è nel vettore degli utenti 
+    return render_template('reg_ko.html')
+
 
 api.run(host="0.0.0.0",port=8085)   
 
