f70618d1ee2eccb9ae41fc561aca1588a8c36962
GimmiPrud <prudenzig@gmail.com>
2024-09-13 16:10:22 +0200

    ...

--- Serverwebfs/server.py
+++ Serverwebfs/server.py
@@ -14,12 +14,12 @@ def indexx():
 
 @api.route('/', methods = ['GET'])
 def regok():
-    return render_template('reg_ok')
+    return render_template('reg_ok.html')
 
 
 @api.route('/', methods = ['GET'])
 def regko():
-    return render_template('reg_ko')
+    return render_template('reg_ko.html')
 
 api.run(host="0.0.0.0",port=8085) 
 
