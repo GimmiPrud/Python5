7cfe92e2b8a253f12a77b59b4bd30ec4da59a9cb
GimmiPrud <prudenzig@gmail.com>
2024-09-17 13:21:01 +0200

    ..

--- Serverwebfs/server.py
+++ Serverwebfs/server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 api = Flask(__name__)
 
-utenti = [['lucio','password1','M','0'],['gianni','passwoe','M','0'],['alessia','delaie','D','0']]
+utenti = [['lucio','password1','0'],['gianni','passwoe','0'],['alessia','delaie','0']]
 
 @api.route('/', methods = ['GET'])
 def index():
@@ -14,12 +14,12 @@ def indexx():
     return render_template('index2.html')
 
 
-@api.route('/ok', methods = ['GET'])
+@api.route('/reg_ok', methods = ['GET'])
 def regok():
     return render_template('reg_ok.html')
 
 
-@api.route('/ko', methods = ['GET'])
+@api.route('/reg_ko', methods = ['GET'])
 def regko():
     return render_template('reg_ko.html')
 
@@ -29,10 +29,19 @@ def registra():
     nome = request.args.get('nome')
     print('Nome inserito', nome)
     password = request.args.get('cognome')
-    # devo prendere tutti i dati e verificare se la terna c'è nel vettore degli utenti 
-    return render_template('reg_ko.html')
 
+    utente = []
+    utente.append(nome)
+    utente.append(password)
+    utente.append("0")
 
-api.run(host="0.0.0.0",port=8085)   
+    if utente in utenti:
+        index = utenti.index(utente)
+        utenti[index] [3] = "1"
+        return render_template('reg_ok.html')
+    else:
+        return render_template('reg_ko.html')
+    
+api.run(host="0.0.0.0",port=8085)  
 
 
