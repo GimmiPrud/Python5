fbc5567afe707f2bbb977811658f637d15e870e0
GimmiPrud <prudenzig@gmail.com>
2024-09-17 13:41:03 +0200

    ......

--- Serverwebfs/server.py
+++ Serverwebfs/server.py
@@ -8,12 +8,6 @@ utenti = [['lucio','password1','0'],['gianni','passwoe','0'],['alessia','delaie'
 def index():
     return render_template('index.html')
 
-
-@api.route('/a', methods = ['GET'])
-def indexx():
-    return render_template('index2.html')
-
-
 @api.route('/reg_ok', methods = ['GET'])
 def regok():
     return render_template('reg_ok.html')
@@ -25,23 +19,10 @@ def regko():
 
 
 @api.route('/registrati', methods = ['GET'])
-def registra():
-    nome = request.args.get('nome')
-    print('Nome inserito', nome)
-    password = request.args.get('cognome')
-
-    utente = []
-    utente.append(nome)
-    utente.append(password)
-    utente.append("0")
-
-    if utente in utenti:
-        index = utenti.index(utente)
-        utenti[index] [3] = "1"
-        return render_template('reg_ok.html')
-    else:
-        return render_template('reg_ko.html')
+def registrati():
+    pass
     
 api.run(host="0.0.0.0",port=8085)  
+
 
 
