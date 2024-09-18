3410f0ce50c498ec659f0bfc0f26b034ec42063a
GimmiPrud <prudenzig@gmail.com>
2024-09-18 13:22:52 +0200

    ,,,

--- Serverwebfs/server.py
+++ Serverwebfs/server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 api = Flask(__name__)
 
-utenti = [['lucio','password1','0'],['gianni','passwoe','0'],['alessia','delaie','0']]
+utenti = [['lucio','password1','0'],['gianni','passwoe','0'],['alessia','delaie','1']]
 
 @api.route('/', methods = ['GET'])
 def index():
@@ -21,7 +21,7 @@ def regko():
 @api.route('/registrati', methods = ['GET'])
 def registrati():
     nome = request.args.get('nome')
-    password = request.args.get('cognome')
+    password = request.args.get('password')
 
     utente = []
     utente.append(nome)
@@ -29,10 +29,12 @@ def registrati():
     utente.append('0')
 
     for u in utenti:
-        if u[0] == utente[0] and u[1]== utente[1] and u[2]==utente[2]:
+        if u[0] == nome and u[1] == password and u[2] == '0':
             return render_template('reg_ok.html')
         else:
             return render_template('reg_ko.html')
+
+
     
 api.run(host="0.0.0.0",port=8085)  
 
