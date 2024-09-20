8f6b8aad9f938e55f7b0ea8a69db82908d74da4d
GimmiPrud <prudenzig@gmail.com>
2024-09-20 11:15:23 +0200

    ...

--- Serverwebfs/server.py
+++ Serverwebfs/server.py
@@ -41,9 +41,15 @@ def accedi():
     nome = request.args.get('nome')
     password = request.args.get('password')
 
-            
+    for utente in utenti:
+        if utente[0]==nome and utente[1]==password and utente[2]=='1':
+            return render_template('reg_ok.html')
+        else:
+            return render_template('reg_ko.html')
 
-api.run(host="0.0.0.0",port=8085) 
+
+if __name__ == '__main__':
+    api.run(host="0.0.0.0",port=8085) 
 
 
 
