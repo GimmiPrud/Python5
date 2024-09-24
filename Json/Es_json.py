ee7408f8a6ba6c1a7f68a948ebf29103986bd58e
GimmiPrud <prudenzig@gmail.com>
2024-09-24 18:04:33 +0200

    ...

--- Json/Es_json.py
+++ Json/Es_json.py
@@ -1,2 +1,12 @@
 
-import sys, json
+mydict_1 = { "brand": "Ford",
+"electric": False,
+"year": 1964,
+"colors": ["red", "white", "blue"]}
+
+mydict_2 = "{ 'brand':   'Ford'," + \
+"'electric': False," + \
+"'year': 1964," + \
+"'colors': ['red', 'white', 'blue']}"
+
+
