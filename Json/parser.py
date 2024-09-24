273471a876af214c1d9cd6a13ab7be6736a7980a
GimmiPrud <prudenzig@gmail.com>
2024-09-24 14:58:38 +0200

    ......

---
+++ Json/parser.py
@@ -0,0 +1,23 @@
+
+
+mylist_1 = "[mario, gino, lucrezia]"
+mylist_2 = ["mario","gino","lucrezia"]
+
+var = mylist_1[1]
+print(var)
+type(var)
+
+var_1 = mylist_2[1]
+type(var)
+
+
+def serializza_lista( l_var: list)-> str:
+    pass
+
+
+def deseriallizza_lista(s_var: list)-> list:
+    pass
+
+s_prova = deseriallizza_lista(mylist_1)
+if s_prova == mylist_2:
+    print("procedura avvenuta con successo")
