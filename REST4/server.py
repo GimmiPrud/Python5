8541631e771e55ba6bf1b2ae11ff01442157956d
GimmiPrud <prudenzig@gmail.com>
2024-09-27 17:31:03 +0200

    ....

--- REST4/server.py
+++ REST4/server.py
@@ -1,4 +1,3 @@
-
 from flask import Flask,json,request
 from myJson import JsonSerialize,JsonDeserialize
 
@@ -7,23 +6,31 @@ api = Flask(__name__)
 
 @api.route('/add_cittadino', methods = ['POST'])
 def GestisciAddCittadino():
-    #prendi i dati della richiesta
+#prendi i dati della richiesta
     content_type = request.headers.get('Content-Type')
     print("Ricevuta chiamata " + content_type)
     if content_type == "application/json":
         jRequest = request.json
-        print(jRequest)
-        jResponse = {"Error" : "000" , "Msg": "Ok"}
-        return json.dumps(jResponse),200 #200 è il codice del http
+        sCodiceFiscale = jRequest["codice fiscale"]
+        print("Ricevuto " + sCodiceFiscale)
+        #carichiamo l'anagrafe
+        dAnagrafe = JsonDeserialize(sFileAnagrafe)
+        if sCodiceFiscale not in dAnagrafe:
+            dAnagrafe[sCodiceFiscale] = jRequest
+            JsonSerialize(dAnagrafe,sFileAnagrafe)
+            jResponse = {"Error" : "000" , "Msg": "Ok"}
+            return json.dumps(jResponse),200 #200 è il codice del http
+        else:
+            jResponse = {"Error" : "001" , "Msg": "codice fiscale già presente"}
+            return json.dumps(jResponse),200
     else:
         return "Errore , formato non riconosciuto",401
-#controlla che il cittadino non è gia presente in anagrafe
-#rispondi 
 
+    #controlla che il cittadino non è gia presente in anagrafe
+    #rispondi
 
-
-
-
-
+@api.route('/', methods = ['GET'])
+def manageget():
+    return "Ciao a tutti"
 
 api.run(host = "127.0.0.1" , port = 8080)
