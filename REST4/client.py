ac70ef1f78925b59387583915b1f52a277e741ea
GimmiPrud <prudenzig@gmail.com>
2024-09-27 15:04:44 +0200

    ....

---
+++ REST4/client.py
@@ -0,0 +1,11 @@
+import json, requests
+
+
+def CreaInterfaccia():
+    print("Operazioni disponibili")
+    print("1. Inserisci cittadino (es. atto di nascita)")
+    print("2. Richiedi dati cittadino (es. cert. residenza)")
+    print("3. Modifica dati cittadino")
+    print("4. Elimina Cittadino")
+    print("5. Exit")
+
