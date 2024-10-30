33faac8275b31d09593d4ab6aaaad156f72962c4
GimmiPrud <prudenzig@gmail.com>
2024-10-30 22:55:35 +0100

    .....

---
+++ progetto_flask/es_database_flask/main.py
@@ -0,0 +1,3 @@
+import os # libreria che contiene molte funzioni per interagire con il sistema operativo 
+import sys # libreria che fornisce accesso a variabili e funzioni direttamente dall'interprete python
+import flask 
