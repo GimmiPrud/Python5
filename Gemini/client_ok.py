411b8b6ddaf7e1312d5e299eda5ddbb509d2ffc1
GimmiPrud <prudenzig@gmail.com>
2024-11-05 13:42:39 +0100

    .....

--- Gemini/client_ok.py
+++ Gemini/client_ok.py
@@ -1,88 +1,25 @@
+
+# Utilizzeremo 
+
 import requests, json, sys
 
-base_url = "http://127.0.0.1:8080"
+# recupero un chiave API da google: (per crearla bisogna andare su Google API studio, richiderla e verrà generata)
+# chiave API generata:
+Google_API_KEY = 'AIzaSyDDQ15Eg6rQ-pl6Ed1tn2VxM-HNI7dvnlQ'
+
+sModel = "gemini-1.5-pro-exp-0827" # scegliamo un modello di google gemini 
+base_url = "https://generativelanguage.googleapis.com/v1beta/models/" + sModel + ":generateContent?key="  # creiamo la base dell'Url inserendo anche il modello 
+api_url = base_url + Google_API_KEY 
 
 
-def GetDatiCittadino():
-    nome = input("Inserisci il nome: ")
-    cognome = input("Inserisci il cognome: ")
-    dataN = input("Inserisci la data di nascita (gg/mm/aaaa): ")
-    codF = input("Inserisci il codice fiscale: ")
-    datiCittadino = {
-        "nome": nome, 
-        "cognome": cognome, 
-        "dataNascita": dataN, 
-        "codFiscale": codF
-    }
-    return datiCittadino
-
-
-def GetCodicefiscale():
-    cod = input('Inserisci codice fiscale: ')
-    return {"codFiscale": cod}
-
-
-def EseguiOperazione(iOper, sServizio, dDatiToSend):
-    try:
-        if iOper == 1:
-            response = requests.post(sServizio, json=dDatiToSend)
-        if iOper == 2:
-            response = requests.get(sServizio)
-        if iOper == 3:
-            response = requests.put(sServizio, json=dDatiToSend)
-        if iOper == 4:
-            response = requests.delete(sServizio, json=dDatiToSend)
-
-        if response.status_code==200:
-            print(response.json())
-        else:
-            print("Attenzione, errore " + str(response.status_code))
-    except:
-        print("Problemi di comunicazione con il server, riprova più tardi.")
-
-def EffettuaPrimoLogin():
-    global sUsername, sPassword,sPrivilegio, iPrimoLoginEffettuato 
-
-    #inserisci username
-    sUsername = input("Inserisci username: ")
-
-    #inserisci password
-    sPassword = input("Inserisci password: ")
-
-    #componi jsonRequest
-    jsonRequest = {"username": sUsername, "password":sPassword}
-
-    try:
-        #manda i dati al server
-        api_url = base_url + "/login"
-        response = requests.post(api_url,json=jsonRequest)
-        
-        #processa la risposta del server
-        if response.status_code==200:
-            jsonResponse = response.json
-            if jsonResponse["Esito"]=="000":
-                sPrivilegio = jsonResponse["Privilegio"]
-                iPrimoLoginEffettuato = 1
-    except:
-        print("Attenzione, problemi di comunicazione con il server")
-
-
-print("Benvenuti al Comune - sede locale")
-sUsername=""
-sPassword = ""
-sPrivilegio = ""
-iPrimoLoginEffettuato = 0 
-while iPrimoLoginEffettuato == 0:
-    iPrimoLoginEffettuato = EffettuaPrimoLogin()
+print("Benvenuti su Google Gemini")
 
 iFlag = 0
 while iFlag==0:
     print("\nOperazioni disponibili:")
-    print("1. Inserisci cittadino")
-    print("2. Richiedi cittadino")
-    print("3. Modifica cittadino")
-    print("4. Elimina cittadino")
-    print("5. Esci")
+    print("1. Inserisci una domanda: ")
+    print("2. inserisci una coppia (file, domanda): ")
+    print("3. Esci")
 
 
     try:
@@ -91,39 +28,25 @@ while iFlag==0:
         print("Inserisci un numero valido!")
         continue
 
-
+    iOper = int(input("inserisci opzione: "))
     if iOper == 1:
-        print("Aggiunta cittadino")
-        api_url = base_url + "/add_cittadino"
-        jsonDataRequest = GetDatiCittadino()
-        EseguiOperazione(1, api_url, jsonDataRequest)
+        s_query =input("Cosa vui chiedere ?")
+        jsonDataRequest = {"contents": [{"parts":[{"text": s_query}]}]} # questa è il tipo di domanda che google si aspetta 
+        response = requests.post(api_url, json=jsonDataRequest, verify=True) # facciamo la richiesta (contenuta all'interno del Json)
+        print(response.json())
 
-    # Richiesta dati cittadino
+
     elif iOper == 2:
-        print("Richiesta dati cittadino")
-        api_url = base_url + "/read_cittadino"
-        jsonDataRequest = GetCodicefiscale()
-        EseguiOperazione(2, api_url + "/" + jsonDataRequest['codFiscale'],None)
+        print("servizio da gestire")
+
 
     elif iOper == 3:
-        print("Modifica cittadino")
-        api_url = base_url + "/update_cittadino"
-        jsonDataRequest = GetDatiCittadino()
-        EseguiOperazione(3, api_url, jsonDataRequest)
-
-
-    elif iOper == 4:
-        print("Eliminazione cittadino")
-        api_url = base_url + "/elimina_cittadino"
-        jsonDataRequest = GetCodicefiscale()
-        EseguiOperazione(4, api_url, jsonDataRequest)
-
-    elif iOper == 5:
         print("Buona giornata!")
         iFlag = 1
 
     else:
-        print("Operazione non disponibile, riprova.")
+        print("operazione non disponibile, riprovare")
+
 
 
 
